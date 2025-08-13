4bc32cb7cfc706a8bd7be6b369bd3fe8e34c1c06
Jiyun Hyo <jiyun.hyo@gmail.com>
2025-08-13 19:21:55 -0400

    pushing example

--- 03-task-manager/final/app.js
+++ 03-task-manager/final/app.js
@@ -19,6 +19,9 @@ app.use(notFound);
 app.use(errorHandlerMiddleware);
 const port = process.env.PORT || 5000;
 
+app.use(errorHandlerMiddleware);
+const port = process.env.PORT || 5000;
+
 await connectDB(process.env.MONGO_URI);
 app.listen(port, () => console.log(`Server is listening on port ${port}...`));
 
