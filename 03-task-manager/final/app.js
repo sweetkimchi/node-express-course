7777be5d6de937e468e9d235775f2633407bb243
Jiyun Hyo <jiyun.hyo@gmail.com>
2025-08-13 19:23:31 -0400

    push

--- 03-task-manager/final/app.js
+++ 03-task-manager/final/app.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 require("express-async-errors");
+
 const express = require("express");
 const app = express();
 
@@ -8,11 +9,10 @@ const notFoundMiddleware = require("./middleware/not-found");
 const errorHandlerMiddleware = require("./middleware/error-handler");
 
 app.use(express.json());
-// extra packages
 
 // routes
 app.get("/", (req, res) => {
-  res.send("jobs api");
+  res.send("<h1>Email Project</h1>");
 });
 
 app.use(notFoundMiddleware);
